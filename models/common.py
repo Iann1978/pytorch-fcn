f8c22d4807caf40e28d1cd7b4e03ec6459e8ea12
Iann Dang <iann1978@163.com>
2020-09-08 16:47:48 +0800

    - Abstract models/common.py.

---
+++ models/common.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  8 16:43:49 2020
+
+@author: Iann
+"""
+
+import torch.nn as nn
+
+            
+def autopad(k, p=None):  # kernel, padding
+    # Pad to 'same'
+    if p is None:
+        p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
+    return p
+
+
+class Conv(nn.Module):
+    # Standard convolution
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, act=True):  # ch_in, ch_out, kernel, stride, padding, groups
+        super(Conv, self).__init__()
+        self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p), groups=g, bias=False)
+        self.bn = nn.BatchNorm2d(c2)
+        self.act = nn.LeakyReLU(0.1, inplace=True) if act else nn.Identity()
+
+    def forward(self, x):
+        return self.act(self.bn(self.conv(x)))
+
+    def fuseforward(self, x):
+        return self.act(self.conv(x))
--- train.py
+++ train.py
@@ -15,6 +15,8 @@ import torch
 import torchvision
 import torch.utils.data
 
+from models.common import Conv
+
 device = torch.device("cuda:0")
 
 # x_train = torch.Tensor(np.random.randn(1,3,50,50))
@@ -111,27 +113,7 @@ def fit(eporchs, model, criterion, optimizer,dataloader,debug=False):
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-            
-def autopad(k, p=None):  # kernel, padding
-    # Pad to 'same'
-    if p is None:
-        p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
-    return p
 
-
-class Conv(nn.Module):
-    # Standard convolution
-    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, act=True):  # ch_in, ch_out, kernel, stride, padding, groups
-        super(Conv, self).__init__()
-        self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p), groups=g, bias=False)
-        self.bn = nn.BatchNorm2d(c2)
-        self.act = nn.LeakyReLU(0.1, inplace=True) if act else nn.Identity()
-
-    def forward(self, x):
-        return self.act(self.bn(self.conv(x)))
-
-    def fuseforward(self, x):
-        return self.act(self.conv(x))
 
 class Net(nn.Module):
     def __init__(self):
