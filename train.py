8d1d9af3f6f9c49213fbf3d9202b80a07c6dcd96
Iann Dang <iann1978@163.com>
2020-09-08 16:38:26 +0800

    + Abstract common class Conv.

--- train.py
+++ train.py
@@ -111,23 +111,48 @@ def fit(eporchs, model, criterion, optimizer,dataloader,debug=False):
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
+            
+def autopad(k, p=None):  # kernel, padding
+    # Pad to 'same'
+    if p is None:
+        p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
+    return p
+
+
+class Conv(nn.Module):
+    # Standard convolution
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, act=True):  # ch_in, ch_out, kernel, stride, padding, groups
+        super(Conv, self).__init__()
+        self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p), groups=g, bias=False)
+        self.bn = nn.BatchNorm2d(c2)
+        self.act = nn.LeakyReLU(0.1, inplace=True) if act else nn.Identity()
+
+    def forward(self, x):
+        return self.act(self.bn(self.conv(x)))
+
+    def fuseforward(self, x):
+        return self.act(self.conv(x))
 
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         
-        self.conv1_1 = nn.Conv2d(3, 64, 3,padding=1)
-        self.relu1_1 = nn.ReLU(inplace=True)
-        self.conv1_2 = nn.Conv2d(64, 64, 3,padding=1)
-        self.relu1_2 = nn.ReLU(inplace=True)
+        self.conv1 = Conv(3,64,3,1)
+        
+        #self.conv1_1 = nn.Conv2d(3, 64, 3,padding=1)
+        #self.relu1_1 = nn.ReLU(inplace=True)
+        #self.conv1_2 = nn.Conv2d(64, 64, 3,padding=1)
+        #self.relu1_2 = nn.ReLU(inplace=True)
         self.pool1 = nn.MaxPool2d(2, 2)  # 1/2
         
         
          # conv2
-        self.conv2_1 = nn.Conv2d(64, 128, 3, padding=1)
-        self.relu2_1 = nn.ReLU(inplace=True)
-        self.conv2_2 = nn.Conv2d(128, 1, 3, padding=1)
-        self.relu2_2 = nn.ReLU(inplace=True)
+        # self.conv2_1 = nn.Conv2d(64, 128, 3, padding=1)
+        # self.relu2_1 = nn.ReLU(inplace=True)
+        # self.conv2_2 = nn.Conv2d(128, 1, 3, padding=1)
+        # self.relu2_2 = nn.ReLU(inplace=True)
+        
+        self.conv2 = Conv(64,1,3,1)
         self.pool2 = nn.MaxPool2d(2,2)  # 1/4
         
         
@@ -135,16 +160,21 @@ class Net(nn.Module):
         self.upsam2 = nn.Upsample(scale_factor=2)
 
     def forward(self, x):
-        x = self.conv1_1(x)
-        x = self.relu1_1(x)
-        x = self.conv1_2(x)
-        x = self.relu1_2(x)
+        # x = self.conv1_1(x)
+        # x = self.relu1_1(x)
+        # x = self.conv1_2(x)
+        # x = self.relu1_2(x)
+        # x = self.pool1(x)
+        
+        x = self.conv1(x)
         x = self.pool1(x)
         
-        x = self.conv2_1(x)
-        x = self.relu2_1(x)
-        x = self.conv2_2(x)
-        x = self.relu2_2(x)
+        # x = self.conv2_1(x)
+        # x = self.relu2_1(x)
+        # x = self.conv2_2(x)
+        # x = self.relu2_2(x)
+        
+        x = self.conv2(x)
         x = self.pool2(x)
         
         x = self.upsam1(x)
